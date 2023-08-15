ec66695f90d0988385d46b0cc9dfc5f9b4a60dae
Jack <jackbadawy1999@gmail.com>
2023-08-15 16:13:21 +1000

    attempt to commit repository

---
+++ api_infograbber.js
@@ -0,0 +1,103 @@
+const apiMainEl = document.getElementById("api-main-el");
+let mainPara = document.getElementById("main-para"); //detlete if not used
+let pokeApi = "https://pokeapi.co/api/v2/pokemon/"; //eventually we're going to plus equals the specific pokemon we want
+let pokeSpeciesUrl;
+let pokeData;
+let pokeSpeciesData;
+apiInitialize(); //function needs to be triggered above these element grabbers
+let searchBtn = document.getElementById("search-btn");
+let searchBar = document.getElementById("search-bar");
+let searchContainer = document.getElementById("search-container");
+let infoGenerated = false;
+
+function apiInitialize() {
+  // this funciton is going to add the search bar, button and other related elements to clean up the top a bit
+  //const searchForm = document.createElement("form");
+  const searchBar = document.createElement("input");
+  const searchBtn = document.createElement("button");
+  const searchContainer = document.createElement("div");
+  searchBar.type = "text";
+  searchBar.placeholder = "Search a Pokemon!";
+  searchBar.id = "search-bar";
+  searchBtn.textContent = "Search";
+  searchBtn.id = "search-btn";
+  searchContainer.id = "search-container";
+
+  //apiMainEl.insertBefore(searchForm, apiMainEl.lastChild);
+  apiMainEl.insertBefore(searchContainer, apiMainEl.lastChild);
+  searchContainer.insertBefore(searchBtn, searchContainer.lastChild);
+  searchContainer.insertBefore(searchBar, searchContainer.lastChild);
+}
+
+searchBtn.addEventListener("click", function () {
+  pokeApi = "https://pokeapi.co/api/v2/pokemon/" + searchBar.value;
+
+  getData();
+});
+
+async function getData() {
+  const response = await fetch(pokeApi);
+  pokeData = await response.json();
+  console.log(pokeData.species.name); //successfully returns ditto comment out later
+  pokeSpeciesUrl = pokeData.species.url;
+  //getMoreData();
+  const responseTwo = await fetch(pokeSpeciesUrl);
+  pokeSpeciesData = await responseTwo.json();
+  displayInfo();
+}
+
+async function getMoreData() {
+  //delete
+  /* const response = await fetch(pokeSpeciesUrl);
+  pokeSpeciesData = await response.json(); */
+}
+
+function displayInfo() {
+  if (infoGenerated === true) {
+    document.getElementById("master-div").remove();
+  }
+  infoGenerated = true;
+  const masterDiv = document.createElement("div");
+  const infoDiv = document.createElement("div"); //make another div container so the image can be next to the general info
+  const nameParagraph = document.createElement("p");
+  let evolvesFrom = false;
+
+  masterDiv.id = "master-div";
+  infoDiv.id = "info-div";
+  nameParagraph.id = "name-paragraph";
+
+  nameParagraph.innerHTML = "<b>Name: </b>" + pokeData.species.name;
+  apiMainEl.insertBefore(masterDiv, apiMainEl.lastChild);
+  masterDiv.insertBefore(infoDiv, masterDiv.lastChild);
+  infoDiv.insertBefore(nameParagraph, infoDiv.lastChild);
+
+  if (pokeSpeciesData.evolves_from_species) {
+    const evolvesFromParagraph = document.createElement("p");
+    evolvesFromParagraph.id = "evolves-from-paragraph";
+    evolvesFromParagraph.innerHTML =
+      "<b>Evolves from:</b> " + pokeSpeciesData.evolves_from_species.name;
+
+    infoDiv.lastChild.after(evolvesFromParagraph); //change apimain el to container maybe?
+  }
+
+  const pokedexNumberParagraph = document.createElement("p");
+  pokedexNumberParagraph.id = "pokedex-number-paragraph";
+  pokedexNumberParagraph.innerHTML =
+    "<b>Pokedex Entry: </b>" + pokeSpeciesData.order;
+
+  infoDiv.lastChild.after(pokedexNumberParagraph);
+
+  const pokeDescription = document.createElement("p");
+  pokeDescription.id = "poke-description";
+  pokeDescription.innerHTML =
+    "<b>Description: </b>" + pokeSpeciesData.flavor_text_entries[0].flavor_text; //looks like an object change to .0
+  infoDiv.lastChild.after(pokeDescription);
+
+  const pokeImage = document.createElement("img");
+  pokeImage.id = "poke-image";
+  pokeImage.src = pokeData.sprites.front_default; // check that we can write .src
+  pokeImage.alt = searchBar.value;
+  masterDiv.lastChild.after(pokeImage);
+}
+
+//everything is now ready to go, just need to add paragraphs to display relevant text
---
+++ tictactoe.js
@@ -0,0 +1,156 @@
+const mainEl = document.getElementById("main-el");
+const messageEl = document.createElement("div");
+messageEl.id = "message-el";
+mainEl.insertBefore(messageEl, mainEl.firstChild);
+let gridStats = Array.from(Array(9), () => new Array(4));
+let gridContainer;
+gridContainer = document.createElement("div");
+gridContainer.id = "grid-container";
+document
+  .getElementById("main-el")
+  .insertBefore(gridContainer, mainEl.nextSibling);
+
+initialize();
+
+function initialize() {
+  for (i = 0; i < gridStats.length; i++) {
+    newDiv = document.createElement("div");
+    newDiv.id = "s" + (i + 1);
+    newDiv.classList.add("ticbox");
+    gridContainer.insertBefore(newDiv, gridContainer.nextSibling);
+    newP = document.createElement("p");
+    newP.id = newDiv.id + "-text";
+    newDiv.insertBefore(newP, newDiv.nextSibling);
+    gridStats[i][2] = document.getElementById(`s${i + 1}-text`);
+  }
+  playersTurn();
+}
+
+function createGridContainer() {}
+
+function restartGame() {
+  document.getElementById("grid-container").remove();
+  messageEl.textContent = undefined;
+  gridStats = Array.from(Array(9), () => new Array(4));
+  initialize();
+}
+
+function playersTurn() {
+  gridContainer.addEventListener("click", function (e) {
+    if (e?.target?.firstElementChild?.textContent) {
+      console.log("already filled");
+    } else {
+      e.target.firstElementChild.textContent += "X"; // fills p text
+      update("1");
+    }
+  });
+}
+
+function lockout() {
+  p = false;
+  i = 0;
+
+  while (p === false) {
+    i = i + 1;
+    a = Math.floor(Math.random() * 9);
+
+    if (!gridStats[a][3]) gridStats[a][2].textContent = " ";
+
+    if (i === 99) {
+      p = true;
+      playersTurn();
+    }
+  }
+}
+
+function update(carry) {
+  for (i = 0; i < gridStats.length; i++) {
+    if (gridStats[i][2].textContent === "X") gridStats[i][3] = "X";
+
+    if (gridStats[i][2].textContent === "O") gridStats[i][3] = "O";
+  }
+
+  a = carry;
+  winChecker(a);
+}
+
+function computersTurn() {
+  p = false;
+  i = 0;
+  while (p === false) {
+    i = i + 1;
+    a = Math.floor(Math.random() * 9);
+    if (gridStats[a][3]) {
+    } else {
+      p = true;
+      gridStats[a][2].textContent += "O";
+    }
+    if (i === 99) {
+      p = true;
+      console.log("tie");
+      messageEl.textContent = "Tie!!!";
+      playersTurn();
+    }
+  }
+  update("2");
+}
+
+function winChecker(a) {
+  if (
+    (gridStats[0][3] === "X" &&
+      gridStats[1][3] === "X" &&
+      gridStats[2][3] === "X") ||
+    (gridStats[0][3] === "X" &&
+      gridStats[4][3] === "X" &&
+      gridStats[8][3] === "X") ||
+    (gridStats[0][3] === "X" &&
+      gridStats[3][3] === "X" &&
+      gridStats[6][3] === "X") ||
+    (gridStats[1][3] === "X" &&
+      gridStats[4][3] === "X" &&
+      gridStats[7][3] === "X") ||
+    (gridStats[2][3] === "X" &&
+      gridStats[5][3] === "X" &&
+      gridStats[8][3] === "X") ||
+    (gridStats[2][3] === "X" &&
+      gridStats[4][3] === "X" &&
+      gridStats[6][3] === "X") ||
+    (gridStats[3][3] === "X" &&
+      gridStats[4][3] === "X" &&
+      gridStats[5][3] === "X")
+  ) {
+    console.log("you Win!!!");
+    messageEl.textContent = "You Win!!!";
+    lockout();
+  } else if (
+    (gridStats[0][3] === "O" &&
+      gridStats[1][3] === "O" &&
+      gridStats[2][3] === "O") ||
+    (gridStats[0][3] === "O" &&
+      gridStats[4][3] === "O" &&
+      gridStats[8][3] === "O") ||
+    (gridStats[0][3] === "O" &&
+      gridStats[3][3] === "O" &&
+      gridStats[6][3] === "O") ||
+    (gridStats[1][3] === "O" &&
+      gridStats[4][3] === "O" &&
+      gridStats[7][3] === "O") ||
+    (gridStats[2][3] === "O" &&
+      gridStats[5][3] === "O" &&
+      gridStats[8][3] === "O") ||
+    (gridStats[2][3] === "O" &&
+      gridStats[4][3] === "O" &&
+      gridStats[6][3] === "O") ||
+    (gridStats[3][3] === "O" &&
+      gridStats[4][3] === "O" &&
+      gridStats[5][3] === "O")
+  ) {
+    console.log("You Lose");
+    messageEl.textContent = "You Lose!!!";
+    lockout();
+  } else if (a === "1") {
+    computersTurn();
+  } else {
+    playersTurn();
+  }
+}
